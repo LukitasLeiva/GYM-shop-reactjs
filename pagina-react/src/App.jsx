4ebfab80cecf92bb417a1d468a64de3e203390a7
Lucas Leiva <leivalucas0903@gmail.com>
2024-03-07 20:54:57 -0300

    arreglo de carga

--- pagina-react/src/App.jsx
+++ pagina-react/src/App.jsx
@@ -1,7 +1,6 @@
 import Header from './components/Header';
 import Footer from './components/Footer';
 import ItemListContainer from './components/ItemListContainer';
-import Productos from './components/Productos';
 import{ BrowserRouter, Routes, Route } from "react-router-dom";
 import ItemDetailContainer from "./components/ItemDetailContainer";
 
@@ -11,7 +10,6 @@ function App() {
   return (
     <BrowserRouter>
       <Header />
-      <Productos />
       <Routes>
         <Route path={"/"} element={<ItemListContainer/>}/>
         <Route path={"/category/:id"} element={<ItemListContainer/>}/>
